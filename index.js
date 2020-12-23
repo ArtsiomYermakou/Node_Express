13466b1b832da68c1400af87f9ff46217e6629cc
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-12-23 18:01:44 +0300

    Add dynamic basket

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
 const homeRoutes = require("./routes/home")
+const cardRoutes = require("./routes/card")
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
+const path = require("path")
 
 const hbs = exphbs.create({
     defaultLayout: "main",
@@ -15,11 +17,12 @@ app.set("view engine", "hbs");
 app.set("views", "views");
 
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use("/", homeRoutes);
 app.use("/add", addRoutes);
 app.use("/courses", coursesRoutes);
+app.use("/card", cardRoutes);
 /////
 
 
