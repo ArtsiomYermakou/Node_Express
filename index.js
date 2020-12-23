636b4909a5b3c2b9d2597ac1d9d01e9b11d21051
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-12-23 12:45:58 +0300

    Add routes

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home")
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
 
 const hbs = exphbs.create({
     defaultLayout: "main",
     extname: "hbs"
 })
+
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
-app.set("views", "views")
+app.set("views", "views");
 
+
+app.use(express.static("public"));
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
 /////
 
-app.get("/", (req, res) => {
-    res.render("index");
-})
-
-app.get("/about", (req, res) => {
-    res.render("about");
-})
 
 const PORT = process.env.PORT || 3000;
 
